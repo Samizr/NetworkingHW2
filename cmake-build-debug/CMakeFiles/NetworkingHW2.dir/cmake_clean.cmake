e266893a3d14ef5413f4fc3c76d302ab033b346b
Sami Zreik <samizr@campus.technion.ac.il>
2018-12-13 20:35:27 +0200

    Test
    Test

---
+++ cmake-build-debug/CMakeFiles/NetworkingHW2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworkingHW2.dir/main.cpp.obj"
+  "NetworkingHW2.pdb"
+  "NetworkingHW2.exe"
+  "NetworkingHW2.exe.manifest"
+  "libNetworkingHW2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NetworkingHW2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
